618d9e5e38a78d759558d1014e67f01068d0f2ce
William Homer <billyhomeriii@gmail.com>
2019-07-08 09:57:17 -0600

    add food poro

---
+++ app/models/food.rb
@@ -0,0 +1,15 @@
+class Food
+  attr_reader :ndb,
+              :name,
+              :group,
+              :data_source
+              :manufacturer
+
+  def initialize(attributes)
+    @ndb = attributes[:ndbno]
+    @name = attributes[:name]
+    @group = attributes[:group]
+    @data_source = attributes[:ds]
+    @manufacturer = attributes[:manu]
+  end
+end
